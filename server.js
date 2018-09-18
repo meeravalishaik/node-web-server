8c5bd3bf40b2f20b02e187a4fcdaf61e1d5e6505
Meeravali Shaik -X (meershai - TATA CONSULTANCY SERVICES LIMITED at Cisco) <meershai@partnet.cisco.com>
2018-09-18 17:21:20 +0530

    deploy commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 let app = express();
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.port || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -18,9 +19,9 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use((req, res, next) => {
-    res.render('maintanence.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintanence.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -47,6 +48,6 @@ app.get('/bad', (req, res) => {
         error: "Bad Request 404"
     })
 })
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
